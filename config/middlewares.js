3cd2f01d3adc9521554d1ec209bf3ce53866cd7a
Cornu Noah <noah.cornu@icloud.com>
2024-09-20 11:20:49 +0200

    ajout cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: [
+        "https://portfolio-emcuxkdnj-noahs-projects-19610baa.vercel.app",
+      ], // Remplace par ton domaine
+    },
+  },
+};
