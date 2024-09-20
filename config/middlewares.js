d8d7a283fc1836b542efb251888d8efec2e0674b
Cornu Noah <noah.cornu@icloud.com>
2024-09-20 11:39:52 +0200

    test 5

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "http://localhost:3000",
+        "https://portfolio-gz2htvqne-noahs-projects-19610baa.vercel.app/",
+      ], // Ajouter les domaines autorisés
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
