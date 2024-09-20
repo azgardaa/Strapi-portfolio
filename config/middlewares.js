2aa124dcdfeba09abd0fdb948474821dfa793e47
Cornu Noah <noah.cornu@icloud.com>
2024-09-20 11:45:35 +0200

    changement

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,18 +2,7 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "http://localhost:3000",
-        "https://portfolio-taupe-eight-29.vercel.app/",
-      ], // Ajouter les domaines autorisés
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
